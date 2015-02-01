978f00b4b95578bbb89eb2951b79d796a59bc6de
krishna <kcparashar@gmail.com>
2015-02-01 14:51:22 -0800

    Figuring out routing, figure out PostgreSQL next

---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello World\n');
+}).listen(3000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:3000/'); 
+
+/* var express = require('express');
+
+var app = express();
+
+
+
+
+
+app.listen(3000);
+console.log('Listening on port 3000...'); */
