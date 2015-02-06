945faf71a3252c5b5e3be3b2e0106250270f9a50
krishna <kcparashar@gmail.com>
2015-02-06 00:26:50 -0800

    Figured out configurations, implementing DB

---
+++ app.js
@@ -0,0 +1,94 @@
+/*
+var http = require('http');
+var pg = require('pg');
+var express = require('express');
+var routes = require('./routes');
+
+var user = require('./routes/user'); // What is this for?
+var path = require('path');          // What is this for?
+
+var port = 3000;
+var host = '127.0.0.1';
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+app.listen(3000);
+*/
+
+/* AUTOMATICALLY GENERATED */
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
+
+/* SIMPLE HELLO WORLD
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
+*/
+
+
+/* POSTGRES SIMPLE
+var conString = "postgres://username:password@localhost/database";
+
+pg.connect(conString, function(err, client, done) {
+
+  if (err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    done();
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+  });
+
+});
+*/
+
