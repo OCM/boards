430ad0ad5119849b64538aa02b46214315fa20bd
krishna <kcparashar@gmail.com>
2015-03-09 09:09:09 -0700

    User Routing

--- app.js
+++ app.js
@@ -1,55 +1,184 @@
+
 /* Requirements */
-var http     = require('http');
-var express  = require('express');
-var pg       = require('pg');
-var path     = require('path'); // What is this for?
-var config   = require('./config');
-
-var port = 3000;
-var host = '127.0.0.1';
-
-var app = express();
-
-//var conString = "pg://krishna:parashar@localhost:5432/OCM";
+var http       = require('http');
+var express    = require('express');
+var bodyParser = require('body-parser');
+var pg         = require('pg');
+var path       = require('path');
+var config     = require('./database/config');
+var _          = require('lodash');
 
 
-var knex = config.dbConfig;
+/* Declarations */
+var host      = '127.0.0.1';
+var port      = 3000;
+var app       = express();
+var knex      = config.dbConfig;
+var bookshelf = config.bookshelf;
+var router    = express.Router();
 
-var bookshelf = require('bookshelf')(knex);
 
+/* Body Parser Middleware */
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); 
+
+
+/* Models */
 var User = bookshelf.Model.extend({
-  tableName: 'users'
+  tableName: 'users',
+  hasTimestamps: true,
+
+  submissions: function () {
+  	return this.belongsTo(Link);
+  },
+  roles: function () {
+  	return this.belongsTo(Role);
+  }
 });
 
 var Link = bookshelf.Model.extend({
-  tableName: 'links'
+  tableName: 'links',
+  hasTimestamps: true,
+
+  tags: function () {
+  	return this.belongsToMany(Tag);
+  },
+  submitter: function () {
+  	return this.belongsTo(Tag);
+  }
 });
 
 var Tag = bookshelf.Model.extend({
-  tableName: 'tags'
+  tableName: 'tags',
+  links: function () {
+  	return this.belongsToMany(Link);
+  },
+  submitter: function () {
+  	return this.belongsTo(User);
+  }
 });
 
-var Application = bookshelf.Model.extend({
-  tableName: 'applications'
+var Role = bookshelf.Model.extend({
+  tableName: 'roles'
 });
 
-var Endpoint = bookshelf.Model.extend({
-  tableName: 'endpoints'
+
+/* Collections */
+var Users = bookshelf.Collection.extend({
+	model: User
 });
 
-app.listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+var Links = bookshelf.Collection.extend({
+	model: Link
+});
+
+var Tags = bookshelf.Collection.extend({
+	model: Tags
+});
+
+var Roles = bookshelf.Collection.extend({
+	model: Role
+});
+
+
+  /* User Routing */
+  router.route('/users')
+    .get(function (req, res) {
+      Users.forge()
+      .fetch()
+      .then(function (collection) {
+        res.json({error: false, data: collection.toJSON()});
+      })
+      .otherwise(function (err) {
+        res.status(500).json({error: true, data: {message: err.message}});
+      });
+    })
+
+  router.route('/users/:id')
+    .get(function (req, res) {
+      User.forge({id: req.params.id})
+      .fetch()
+      .then(function (user) {
+        if (!user) {
+          res.status(404).json({error: true, data: {}});
+        }
+        else {
+          res.json({error: false, data: user.toJSON()});
+        }
+      })
+      .otherwise(function (err) {
+        res.status(500).json({error: true, data: {message: err.message}});
+      });
+    })
+
+    .post(function (req, res) {
+      User.forge({
+        name: req.body.name,
+        password: req.body.email,
+        email: req.body.email
+      })
+      .save()
+      .then(function (user) {
+        res.json({error: false, data: {id: user.get('id')}});
+      })
+      .otherwise(function (err) {
+        res.status(500).json({error: true, data: {message: err.message}});
+      }); 
+    });
+
+
+
+    // update user details
+    .put(function (req, res) {
+      User.forge({id: req.params.id})
+      .fetch({require: true})
+      .then(function (user) {
+        user.save({
+          name: req.body.name || user.get('name'),
+          email: req.body.email || user.get('email'),
+          bio: req.body.bio   || user.get('bio'),
+          website: req.body.website || user.get('website')
+        })
+        .then(function () {
+          res.json({error: false, data: {message: 'User Updated'}});
+        })
+        .otherwise(function (err) {
+          res.status(500).json({error: true, data: {message: err.message}});
+        });
+      })
+      .otherwise(function (err) {
+        res.status(500).json({error: true, data: {message: err.message}});
+      });
+    })
+
+    // delete a user
+    .delete(function (req, res) {
+      User.forge({id: req.params.id})
+      .fetch({require: true})
+      .then(function (user) {
+        user.destroy()
+        .then(function () {
+          res.json({error: true, data: {message: 'User Deleted'}});
+        })
+        .otherwise(function (err) {
+          res.status(500).json({error: true, data: {message: err.message}});
+        });
+      })
+      .otherwise(function (err) {
+        res.status(500).json({error: true, data: {message: err.message}});
+      });
+    });
 
 
 
 
-
-/* Routing Modules */
-var routes   = require('./routes');
-var links    = require('./routes/links');
-var tags     = require('./routes/tags');
-var users    = require('./routes/users');
-// var permissions = require('./routes/applications');
+/* Routing */
+// var routes   = require('./routes');
+// var links    = require('./routes/links');
+// var tags     = require('./routes/tags');
+// var users    = require('./routes/users');
+// var roles    = require('./routes/roles');
+// var permissions  = require('./routes/applications');
 // var applications = require('./routes/applications');
 //var scope = require('./routes/endpoints');
 
@@ -73,3 +202,7 @@ app.get('/orderList', routes.auth, routes.orderList);
 app.post('/orderConfirm', routes.auth, routes.orderconfirm);
 app.get('/orderExecute', routes.auth, routes.orderExecute);
 */
+
+app.use('/', router);
+app.listen(port, host);
+console.log('Server running at %d:%d in %s mode', host, port, app.get('env'));
--- database/schema.js
+++ database/schema.js
@@ -1,48 +1,57 @@
 var schema = {
     links: {
-      id: {type: 'increments', nullable: false, primary: true},
+      id: {type: 'bigIncrements', nullable: false, primary: true},
       uuid: {type: 'string', maxlength: 36, nullable: false},
       title: {type: 'string', maxlength: 150, nullable: false},
-      upvotes: {},
-      downvotes: {},
+      upvotes: {type: 'integer', maxlength: 65535, nullable: true},
+      downvotes: {type: 'integer', maxlength: 65535, nullable: true},
       meta_title: {type: 'string', maxlength: 150, nullable: true},
       meta_description: {type: 'string', maxlength: 200, nullable: true},
-      submitter: {type: 'integer', nullable: false},
+      submitter: {type: 'integer', nullable: false}, // references user
       created_at: {type: 'dateTime', nullable: false},
       updated_at: {type: 'dateTime', nullable: true},
       status: {type: 'string', maxlength: 150, nullable: false, defaultTo: 'review'},
       language: {type: 'string', maxlength: 6, nullable: false, defaultTo: 'en_US'},
-      featured: {type: 'bool', nullable: false, defaultTo: false}
+      tags: {type: 'integer', nullable: false} // references tags
+    },
+    tags: {
+      id: {type: 'increments', nullable: false, primary: true},
+      uuid: {type: 'string', maxlength: 36, nullable: false},
+      name: {type: 'string', maxlength: 150, nullable: false},
+      description: {type: 'string', maxlength: 200, nullable: true},
+      links: {type: 'integer', nullable: true}, // references link(s)
+      submitter: {type: 'string', nullable: false} // references user
     },
     users: {
       id: {type: 'increments', nullable: false, primary: true},
       uuid: {type: 'string', maxlength: 36, nullable: false},
       name: {type: 'string', maxlength: 150, nullable: false, unique: true},
+      username: {type: 'string', maxlength: 150, nullable: false, unique: true},
       password: {type: 'string', maxlength: 60, nullable: false},
       email: {type: 'string', maxlength: 254, nullable: false, unique: true},
       image: {type: 'text', maxlength: 2000, nullable: true},
       bio: {type: 'string', maxlength: 200, nullable: true},
+      karma: {type: 'bigInteger', maxlength: 65535, nullable: true},
       website: {type: 'text', maxlength: 2000, nullable: true},
       location: {type: 'text', maxlength: 65535, nullable: true},
-      status: {type: 'string', maxlength: 150, nullable: false, defaultTo: 'active'},
       language: {type: 'string', maxlength: 6, nullable: false, defaultTo: 'en_US'},
-      meta_title: {type: 'string', maxlength: 150, nullable: true},
-      meta_description: {type: 'string', maxlength: 200, nullable: true},
       last_login: {type: 'dateTime', nullable: true},
       created_at: {type: 'dateTime', nullable: false},
       updated_at: {type: 'dateTime', nullable: true},
-      submissions: {},
-      permissions: {},
+      submissions: {type: 'integer', nullable: true}, // references links
+      roles: {type: 'string', maxlength: 150, nullable: false, defaultTo: 'active'},
     },
     roles: {
       id: {type: 'increments', nullable: false, primary: true},
       uuid: {type: 'string', maxlength: 36, nullable: false},
       name: {type: 'string', maxlength: 150, nullable: false},
-      description: {type: 'string', maxlength: 200, nullable: true},
-      created_at: {type: 'dateTime',  nullable: false},
-      created_by: {type: 'integer',  nullable: false},
-      updated_at: {type: 'dateTime',  nullable: true},
-      updated_by: {type: 'integer',  nullable: true}
+      description: {type: 'string', maxlength: 200, nullable: true}
+    }
+    /* TODO: Permissions and Defined Roles
+    roles_users: {
+      id: {type: 'increments', nullable: false, primary: true},
+      role_id: {type: 'integer', nullable: false},
+      user_id: {type: 'integer', nullable: false}
     },
     permissions: {
       id: {type: 'increments', nullable: false, primary: true},
@@ -50,88 +59,18 @@ var schema = {
       name: {type: 'string', maxlength: 150, nullable: false},
       object_type: {type: 'string', maxlength: 150, nullable: false},
       action_type: {type: 'string', maxlength: 150, nullable: false},
-      object_id: {type: 'integer', nullable: true},
-      created_at: {type: 'dateTime', nullable: false},
-      created_by: {type: 'integer', nullable: false},
-      updated_at: {type: 'dateTime', nullable: true},
-      updated_by: {type: 'integer', nullable: true}
+      object_id: {type: 'integer', nullable: true}
     },
-    tags: {
+    permissions_users: {
       id: {type: 'increments', nullable: false, primary: true},
-      uuid: {type: 'string', maxlength: 36, nullable: false},
-      name: {type: 'string', maxlength: 150, nullable: false},
-      description: {type: 'string', maxlength: 200, nullable: true},
-      parent_id: {type: 'integer', nullable: true},
-      links: {},
-      created_at: {type: 'dateTime', nullable: false},
-      created_by: {type: 'string', nullable: false},
-    }
+      user_id: {type: 'integer', nullable: false},
+      permission_id: {type: 'integer', nullable: false}
+    },
+    permissions_roles: {
+      id: {type: 'increments', nullable: false, primary: true},
+      role_id: {type: 'integer', nullable: false},
+      permission_id: {type: 'integer', nullable: false}
+    }*/
   };
 
 module.exports = schema;
-
-/*
-
-
-
-
-    Links have:
-        id *[bigserial]
-        name [varchar(1024)]
-        link [varchar(1024)]
-        upvotes [integer]
-        downvotes [integer]
-        tags -> reference to tag
-        submitter -> reference to user
-        timestamp [timestamp(0)]
-
-    Users have:
-        id *[serial]
-        username [varchar(255)]
-        email [varchar(255)]
-        password [char(82)]
-        description [text]
-        website [varchar(1024)]
-        karma [bigint]
-        creation [timestamp(0)]
-        submitted links -> reference to links
-        rank [???]
-        api_token [char(82)]
-        Applications -> reference to applications
-
-    Tags have:
-        id *[serial]
-        links -> reference to links
-        description [varchar(255)]
-        title [varchar(255)]
-
-    Applications have:
-        id *[serial]
-        Name [varchar(255)]
-        Description [text]
-        Requested endpoints -> reference to endpoints
-
-    Endpoints have:
-        Name
-        Purpose
-
-* means primary key
-
-You might consider using UUIDs for links, users, and tags to allow the database to be sharded properly in the future.
-
-How do you plan on doing references to tags? Just an array of IDs?
-
-Assumed 256-bit scrypt encoded in base64 for passwords. Length should be updated appropriately for different approaches.
-
-For user access control, you may want to consider both text-based and integer-based solutions.
-
-Karma has been specified as a bigint for scaling purposes, but this will likely be incompatible with your code. I think you mentioned using something like Django, so this may need to be integer type.
-
-API access should be done with tokens or ssh keys, so I've assumed a scrypt hash for the "API User?" field. False would correspond with an empty field.
-
-How many tags are we talking? If it's fewer than a thousand, a bitmap would be a really intelligent and efficient approach, which would allow you to do some sophisticated data analysis for searching and what not.
-
-0001 -> Tag for "Physics"
-0010 -> Tag for "Computer Science"
-0011 -> Tags: Computer Science & Physics.
-*/
