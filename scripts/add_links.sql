337ee5deceb4b8a2eb8c7e3ffa6eecbbab4793ff
Andrew Brinker <me@andrewbrinker.com>
2015-04-01 15:35:50 -0700

    Schema stuff added

---
+++ scripts/add_links.sql
@@ -0,0 +1,21 @@
+INSERT INTO links (short_name, url, title, description, language, upvotes,
+                   downvotes, hotness, status, user_id, created_at,
+                   updated_at) VALUES
+                  ('aaaaaa', 'http://jesus.com', 'Your Lord and Savior',
+                   'Website of Jesus.', 'en_US', 10, 5, 7.5, 'published',
+                   3, now(), now());
+
+INSERT INTO links (short_name, url, title, description, language, upvotes,
+                   downvotes, hotness, status, user_id, created_at,
+                   updated_at) VALUES
+                  ('aaaaab', 'http://god.com', 'Dad of Your Lord and Savior',
+                   'Website of God.', 'en_US', 12, 2, 10.3, 'published',
+                   3, now(), now());
+
+INSERT INTO links (short_name, url, title, description, language, upvotes,
+                   downvotes, hotness, status, user_id, created_at,
+                   updated_at) VALUES
+                  ('aaaaac', 'http://juan.com', 'Juan!',
+                   'Juan! Juan! Juan!', 'en_US', 30, 30, 3.41519, 'published',
+                   2, now(), now());
+
