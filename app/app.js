87ba140f97f1c5cbeb5637b9e8e0077b0cab139d
Andrew Brinker <me@andrewbrinker.com>
2015-04-02 12:14:55 -0700

    Basic routing works

--- app/app.js
+++ app/app.js
@@ -1,28 +1,18 @@
-var http       = require('http');
 var express    = require('express');
 var bodyParser = require('body-parser');
-var pg         = require('pg');
-var Lazy       = require('lazy.js');
+var routes     = require('./routes/routes.js');
 
 var host      = '127.0.0.1';
 var port      = 3000;
-
 var app       = express();
-var router    = express.Router();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-router.use(function (req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
+routes.forEach(function (route) {
+    app.use(route.path, route.route);
 });
 
-router.get('/', function (req, res) {
-  res.send('hello world');
-});
-
-app.use('/', router);
 app.listen(port, host, function () {
   console.log('Server live at %s:%d in %s mode', host, port, app.get('env'));
 });
