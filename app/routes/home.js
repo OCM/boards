904dc581c2537c1aa07ab0a94e815d5d7402f618
Andrew Brinker <me@andrewbrinker.com>
2015-04-02 14:03:26 -0700

    Working on database stuff

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,11 +1,27 @@
 var express = require('express');
-var router = express.Router();
+var pg      = require('pg');
+var router  = express.Router();
 
 var db   = require("../../db.json");
 var conn = "postgres://" + db.user + ":" + db.pass + "@localhost/atrus";
 
 router.get('/', function(req, res) {
-  res.render('home', {name: 'Andrew'});
+  pg.connect(conn, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+
+    client.query('SELECT title FROM links;', function (err, result) {
+      done();
+
+      if (err) {
+        return console.error('error running query', err);
+      }
+
+      res.render('home', {links: result.rows});
+      client.end();
+    });
+  });
 });
 
 module.exports = router;
