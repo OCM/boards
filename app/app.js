2309dbcff2b14837ed736dc4259da8f77171376a
Andrew Brinker <me@andrewbrinker.com>
2015-04-02 13:02:33 -0700

    Fixed views

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 var express    = require('express');
+var exphbs     = require('express-handlebars');
 var bodyParser = require('body-parser');
+var morgan     = require('morgan')
 var routes     = require('./routes/routes.js');
 
 // Define basic variables and initialize the app
@@ -7,19 +9,19 @@ var host      = '127.0.0.1';
 var port      = 3000;
 var app       = express();
 
-// Set bodyParser middleware
+// Set middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'))
 
 // Configure handlebars
 var handlebars_config = {
     defaultLayout: 'main',
-    extname:       '.hbs',
-    layoutsDir:    './templates/layouts',
-    partialsDir:   './templates/partials'
+    layoutsDir:    __dirname + '/views/layouts',
+    partialsDir:   __dirname + '/views/partials'
 };
 app.engine('handlebars', exphbs(handlebars_config));
-app.set('views', './templates');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 
 // Attach all routes defined in routes/routes.js
