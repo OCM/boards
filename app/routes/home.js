3468a1abed586866c4c3a975d663e61071e8c9f5
Andrew Brinker <me@andrewbrinker.com>
2015-04-02 13:37:41 -0700

    Added DB connection stuff

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var db   = require("../../db.json");
+var conn = "postgres://" + db.user + ":" + db.pass + "@localhost/atrus";
+
 router.get('/', function(req, res) {
   res.render('home', {name: 'Andrew'});
 });
