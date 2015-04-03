9970bfcecfa2372aa8fbf7f1883792b8c01a3674
Andrew Brinker <me@andrewbrinker.com>
2015-04-02 19:18:08 -0700

    Even more improvements

--- app/db.js
+++ app/db.js
@@ -7,15 +7,9 @@ exports.query = function (query, q_params, cb) {
   if (arguments.length == 3) params = q_params;
   if (arguments.length == 2) cb = q_params;
   db.connect()
-    .then(function (conn) {
-      return conn.query(query, params);
-    }, function (reason) {
-      console.log(reason);
-    })
-    .then(function (data) {
-      cb(data);
-    }, function (reason) {
-      console.log(reason);
-    })
+    .then(function (conn) { return conn.query(query, params); },
+          function (err)  { cb(reason, null);                 })
+    .then(function (data) { cb(null, data);                   },
+          function (err)  { cb(err, null);                    })
     .done();
 };
--- app/routes/home.js
+++ app/routes/home.js
@@ -3,7 +3,11 @@ var db      = require('../db');
 var router  = express.Router();
 
 router.get('/', function(req, res) {
-  db.query("SELECT title FROM links;", function (data) {
+  db.query("SELECT title FROM links;", function (err, data) {
+    if (err) {
+      console.log(reason);
+      return;
+    }
     res.render('home', {links: data});
   });
 });
