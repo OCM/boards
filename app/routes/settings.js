88ac66b52c0f7716433c52c2c5a2c6a8ecefcfc0
Andrew Brinker <me@andrewbrinker.com>
2015-04-03 16:01:48 -0700

    Added page titles

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('settings');
+  res.render('settings', {title: 'Settings'});
 });
 
 module.exports = router;
--- app/routes/tags.js
+++ app/routes/tags.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('tags');
+  res.render('tags', {title: 'Tags'});
 });
 
 module.exports = router;
