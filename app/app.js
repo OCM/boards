d439858273b7d6aa1dacd543c6b0b2c4ca0178fc
Andrew Brinker <me@andrewbrinker.com>
2015-04-02 18:40:45 -0700

    Cleanup

--- app/app.js
+++ app/app.js
@@ -1,28 +1,28 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var body    = require('body-parser');
-var morgan  = require('morgan');
-var routes  = require('./routes.js');
-var config  = require('../package.json').config;
-var app     = express();
+var express    = require('express');
+var exphbs     = require('express-handlebars');
+var body       = require('body-parser');
+var morgan     = require('morgan');
+var routes     = require('./routes.js');
+var config     = require('../package.json').config;
+var app        = express();
+var hbs_config = {
+  defaultLayout: config.defaultLayout,
+  layoutsDir:    config.dirs.layouts,
+  partialsDir:   config.dirs.partials
+};
 
 app.use(body.urlencoded({extended: true}));
 app.use(body.json());
 app.use(morgan('dev'));
-
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main',
-    layoutsDir:    __dirname + config.dirs.layouts,
-    partialsDir:   __dirname + config.dirs.partials
-}));
-app.set('views', __dirname + config.dirs.views);
+app.engine('handlebars', exphbs(hbs_config));
+app.set('views', config.dirs.views);
 app.set('view engine', 'handlebars');
 
 routes.forEach(function (route) {
-    app.use(route.path, route.route);
+  app.use(route.path, route.route);
 });
 
 app.listen(config.site.port, config.site.host, function () {
-  console.log('Server live at %s:%d in %s mode',
+  console.log('Running: %s:%d in %s mode',
     config.site.host, config.site.port, app.get('env'));
 });
--- app/routes/home.js
+++ app/routes/home.js
@@ -7,17 +7,13 @@ var db      = pg(config);
 router.get('/', function(req, res) {
   db.connect()
     .then(function (conn) {
-      // Connection succeeded
       return conn.query('SELECT title FROM links;');
     }, function (reason) {
-      // Connection failed
       console.log(reason);
     })
     .then(function (data) {
-      // Query succeeded
       res.render('home', {links: data});
     }, function (reason) {
-      // Query failed
       console.log(reason);
     })
     .done();
