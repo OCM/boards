f9d5130b112463662c856c1d4d5ee65db15f5a77
Krishna Parashar <me@krishna.im>
2015-04-09 23:03:24 -0700

    This version of the assets works

--- app/routes/tags.js
+++ app/routes/tags.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('tags', {title: 'Tags'});
+  res.render('tags');
 });
 
 module.exports = router;
